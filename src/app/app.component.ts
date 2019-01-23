2bcf77e965acd3208197c47fd63c0c14c3c5747d
Jair Nunes <jair.silva@fcamara.com.br>
2019-01-23 18:56:51 -0200

    ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { load } from '@tensorflow-models/mobilenet';
+import * as ml5 from 'ml5';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,89 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'drogaraiatrain';
+  @ViewChild('videoRef') videoRef;
+  classifier: any;
+  label: any;
+  mobilenet: any;
+
+  ngAfterContentInit(): void {
+    this.startVideo();
+    this.setup();
+  }
+
+  setup(): any {
+    this.mobilenet = ml5.featureExtractor('MobileNet', this.modelReady);
+  }
+
+  detectFrame = (video) => {
+    // model.detect(video).then(predictions => {
+    //   console.log(predictions);
+    //   requestAnimationFrame(() => {
+    //     this.detectFrame(video, model);
+    //   });
+    // });
+  };
+
+  modelReady() {
+    console.log('Model is ready!!!');
+  }
+
+  videoReady() {
+    console.log('Video is ready!!!');
+  }
+
+  whileTraining(loss) {
+    if (loss == null) {
+      console.log('Training Compe');
+      this.classifier.classify(this.gotResults);
+    } else {
+      console.log(loss);
+    }
+  }
+
+
+  gotResults(error, result) {
+    if (error) {
+      console.error(error);
+    } else {
+      this.label = result;
+      this.classifier.classify(this.gotResults);
+    }
+  }
+
+  getFrame(label) {
+    var video = this.videoRef.nativeElement;
+    this.mobilenet = ml5.featureExtractor('MobileNet', this.modelReady);
+    var classifier = this.mobilenet.classification(video, this.videoReady);
+    classifier.addImage(label);
+  }
+
+  train() {
+    this.classifier.train(this.whileTraining);
+  }
+
+  startVideo() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      const webCamPromise = navigator.mediaDevices
+        .getUserMedia({
+          audio: false,
+          video: {
+            facingMode: "user"
+          }
+        })
+        .then(stream => {
+          window["stream"] = stream;
+          this.videoRef.nativeElement.srcObject = stream;
+          return new Promise((resolve, reject) => {
+            this.videoRef.nativeElement.onloadedmetadata = () => {
+              resolve();
+            };
+          });
+        });
+    }
+  }
+
+
 }
+
+
